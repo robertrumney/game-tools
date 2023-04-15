f778f0c27854760d9aa0303baf3d39f983cd6869
Robert Rumney <robertrumney@gmail.com>
2023-04-15 23:06:00 +0200

    Update OptimizationVolume.cs

--- OptimizationVolume.cs
+++ OptimizationVolume.cs
@@ -3,78 +3,61 @@ using System.Collections.Generic;
 
 public class OptimizationVolume : MonoBehaviour 
 {
-    public bool autoSpawnOnStart = false;
-    public bool useSpawning = false;
+    // List of items in the volume
     public GameObject[] items;
-    public List<GameObject> spawnedItems = new List<GameObject>();
+
+    // Whether the volume is activated
     private bool activated = false;
 
     private void Awake () 
     {
+        // Disable the mesh renderer to hide the volume in the scene
         GetComponent<MeshRenderer>().enabled = false;
+        // Set all items to inactive by default
         SetItems(false);
     }
 
     private IEnumerator Start()
     {
-        ProceduralMachine.instance.volumes.Add(this);
-        if (autoSpawnOnStart) AutoSpawn();
+        // Wait for 2 seconds before enabling the collider (prevents clipping issues)
         yield return new WaitForSeconds(2);
-        activated = true;
-        GetComponent<Collider>().enabled = false;
-        yield return null;
+        // Enable the collider
         GetComponent<Collider>().enabled = true;
-    }
-
-    private void AutoSpawn()
-    {
-        ProceduralMachine.instance.SpawnAtPoint(this, transform.position);
+        // Mark the volume as activated
+        activated = true;
     }
 
     private void OnTriggerEnter(Collider other) 
     {
+        // If the player enters the volume and it's activated
         if (other.CompareTag("Player") && activated)
         {
+            // Spawn items if enabled
             if (useSpawning)
             {
-                ProceduralMachine.instance.Do(this);
+                AutoSpawn();
             }
+            // Set all items to active
             SetItems(true);
         }
     }
 
     private void OnTriggerExit(Collider other) 
     {
+        // If the player exits the volume
         if (other.CompareTag("Player"))
         {
+            // Set all items to inactive
             SetItems(false);
         }
     }
 
-    public void ForceClear()
-    {
-        foreach(GameObject go in spawnedItems)
-        {
-            Destroy(go);
-        }
-        spawnedItems.Clear();
-    }
-
     private void SetItems(bool active)
     {
+        // Set all items in the list to the specified active state
         foreach (GameObject item in items)
         {
             if (item) item.SetActive(active);
         }
-    }
-
-    public void SpawnItem(GameObject go)
-    {
-        spawnedItems.Add(go);
-    }
-
-    public bool CannotSpawnMoreItems(ProceduralMachine pm)
-    {
-        return spawnedItems.Count > pm.amount;
     }
 }
